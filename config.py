f8fb23aaeadb6b74fa475c036b1c3cf0be5923a4
meandmymind <beaverloverock@gmail.com>
2020-05-27 11:04:58 +0300

    Change config vars

--- config.py
+++ config.py
@@ -12,9 +12,9 @@ class Config:
     batch_size: int = 64
     key_length: int = 128
     n_epochs: int = 10
-    learning_rate: float = 0.001
+    learning_rate: float = 0.0001
     print_every: int = 10
-    test_every: int = 5
+    test_every: int = 30
     device: torch.device = torch.device('cuda')
 
 
